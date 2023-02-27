390a5f583b3da301d1c3cec626bc909a0f4abf0d
nereidarondon <nereida.rondon5@gmail.com>
2023-02-27 10:23:33 -0500

    deploy to netlify

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/myweatherdashboard.netlify.app/',
+  // base: '/myweatherdashboard.netlify.app/',
   plugins: [react()],
 });
